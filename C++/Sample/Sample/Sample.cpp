3352d57b79fa6319318c34bb40d1f536e86ee55f
r57zone <r57zone@gmail.com>
2017-07-05 16:31:36 +0400

    Added C++ sample driver

---
+++ C++/Sample/Sample/Sample.cpp
@@ -0,0 +1,102 @@
+#include "stdafx.h"
+#include <windows.h>
+
+#define DLLEXPORT extern "C" __declspec(dllexport)
+
+typedef struct _HMDData
+{
+	double	X;
+	double	Y;
+	double	Z;
+	double	Yaw;
+	double	Pitch;
+	double	Roll;
+} THMD, *PHMD;
+
+typedef struct _Controller
+{
+	double	X;
+	double	Y;
+	double	Z;
+	double	Yaw;
+	double	Pitch;
+	double	Roll;
+	DWORD	Buttons;
+	BYTE	Trigger;
+	SHORT	ThumbX;
+	SHORT	ThumbY;
+} TController, *PController;
+
+DLLEXPORT DWORD __stdcall GetHMDData(__out THMD* myHMD);
+DLLEXPORT DWORD __stdcall GetControllersData(__out TController* MyController, __out TController* MyController2);
+DLLEXPORT DWORD __stdcall SetControllerData(__in INT	dwIndex, __in DWORD	MotorSpeed);
+DLLEXPORT DWORD __stdcall SetCentering(__in int dwIndex);
+
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	/*switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}*/
+	return TRUE;
+}
+
+DLLEXPORT DWORD __stdcall GetHMDData(__out THMD* myHMD)
+{
+	myHMD->X = 0;
+	myHMD->Y = 0;
+	myHMD->Z = 0;
+	myHMD->Yaw = 0;
+	myHMD->Pitch = 0;
+	myHMD->Roll = 0;
+
+	return 0;
+}
+
+DLLEXPORT DWORD __stdcall GetControllersData(__out TController* myController, __out TController* myController2)
+{
+	//Controller 1
+	myController->X = 0;
+	myController->Y = 0;
+	myController->Z = 0;
+
+	myController->Yaw = 0;
+	myController->Pitch = 0;
+	myController->Roll = 0;
+
+	myController->Buttons = 0;
+	myController->Trigger = 0;
+	myController->ThumbX = 0;
+	myController->ThumbY = 0;
+
+	//Controller 2
+	myController2->X = 0;
+	myController2->Y = 0;
+	myController2->Z = 0;
+
+	myController2->Yaw = 0;
+	myController2->Pitch = 0;
+	myController2->Roll = 0;
+
+	myController2->Buttons = 0;
+	myController2->Trigger = 0;
+	myController2->ThumbX = 0;
+	myController2->ThumbY = 0;
+
+	return 0;
+}
+
+DLLEXPORT DWORD __stdcall SetControllerData(__in INT	dwIndex, __in DWORD	MotorSpeed)
+{
+	return 0;
+}
+
+DLLEXPORT DWORD __stdcall SetCentering(__in int dwIndex)
+{
+	return 0;
+}
