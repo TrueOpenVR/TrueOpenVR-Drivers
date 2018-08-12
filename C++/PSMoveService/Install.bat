13a2809ff70a823599c939fad5046383423a4a6d
r57zone <r57zone@gmail.com>
2018-08-12 15:38:47 +0400

    PSMoveService x86

--- C++/PSMoveService/Install.bat
+++ C++/PSMoveService/Install.bat
@@ -1,2 +1,10 @@
-copy PSMoveClient_CAPI.dll %WINDIR%\System32
+@echo Off
+
+If Exist "%WINDIR%\SysWOW64" (
+   copy lib\x64\release\PSMoveClient_CAPI.dll %WINDIR%\System32
+   copy lib\x86\release\PSMoveClient_CAPI.dll %WINDIR%\SysWOW64
+) Else (
+   copy lib\x86\release\PSMoveClient_CAPI.dll %WINDIR%\System32
+)
+echo Installed
 pause
